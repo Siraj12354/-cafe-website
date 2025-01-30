61365562833fe7b19285598ca4c959f1436cb3de
Siraj12354 <sirajdhakal.52@gmail.com>
2025-01-30 16:18:43 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('teaOptions').addEventListener('change', function() {
+    alert('You selected: ' + this.value);
+  });
+  
+  document.getElementById('coffeeOptions').addEventListener('change', function() {
+    alert('You selected: ' + this.value);
+  });
+  
+  document.getElementById('burgerOptions').addEventListener('change', function() {
+    alert('You selected: ' + this.value);
+  });
+  
+  document.getElementById('cakeOptions').addEventListener('change', function() {
+    alert('You selected: ' + this.value);
+  });
